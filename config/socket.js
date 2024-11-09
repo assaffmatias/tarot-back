e7977433ab0d823ce95cf817c1ff9692667a4553
Matias <103156161+assaffmatias@users.noreply.github.com>
2024-11-09 00:42:40 -0300

    nuevos cambios

--- config/socket.js
+++ config/socket.js
@@ -2,9 +2,21 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
+const userConnectedToApp = new Map();
+const userMap_inChat = new Map();
 const app = express();
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {});
 
-module.exports = { app, httpServer, io };
+module.exports = {
+  app,
+  httpServer,
+  io,
+  getSocketId_connected: (userId) => userConnectedToApp.get(userId),
+  setSocketId_connected: (userId, socketId) => userConnectedToApp.set(userId, socketId),
+  removeSocketId_connected: (userId) => userConnectedToApp.delete(userId),
+  getSocketId_inChat: (userId) => userMap_inChat.get(userId),
+  setSocketId_inChat: (userId, socketId) => userMap_inChat.set(userId, socketId),
+  removeSocketId_inChat: (userId) => userMap_inChat.delete(userId),
+};
--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -1,5 +1,15 @@
 const { Transaction } = require("../models");
 const axios = require("axios");
+const {notification : notificationController } = require("./index");
+const {
+  io,
+  getSocketId_connected,
+  setSocketId_connected,
+  removeSocketId_connected,
+  getSocketId_inChat,
+  setSocketId_inChat,
+  removeSocketId_inChat,
+} = require("../config");
 
 const PAYPAL_CLIENT =
   "ASop4SvcdJt7V8LuLD_5xp_vZ5H-4d8nR1N13TIdXwbbUvvM6P7GucdbhXxYfZTye_LNGIQPfqBE-AVs";
@@ -31,6 +41,10 @@ module.exports = {
 
       // Crear el pago con los datos proporcionados por el cliente
       const { amount, currency } = req.body;
+      
+      // console.log('BODY:', req.body);
+      
+      
       const paymentData = {
         intent: "CAPTURE",
         purchase_units: [
@@ -64,6 +78,22 @@ module.exports = {
         (link) => link.rel === "approve"
       ).href;
 
+      // // Crear una notificación para el cliente
+      // const messageClient = `Tu transacción de ${amount} ${currency} está pendiente.`;
+      // const notificationClient = await notificationController.createNotification({
+      //   user: client,
+      //   type: 0, // Tipo de notificación (puedes modificar esto según el tipo de notificación que quieras)
+      //   message: messageClient,
+      // });
+
+      // // Crear una notificación para el vendedor
+      // const messageSeller = `El cliente ${client} ha realizado una compra de ${amount} ${currency}.`;
+      // const notificationSeller = await notificationController.createNotification({
+      //   user: seller,
+      //   type: 0, // Puedes usar otro tipo si es necesario
+      //   message: messageSeller,
+      // });
+
       // Responder con el approval_url para abrirlo en el frontend
       res.status(200).json({ approvalUrl });
     } catch (error) {
@@ -74,7 +104,33 @@ module.exports = {
   registerSuccesTransaction: async (req, res, next) => {
     try {
       const create = await Transaction.create(req.body);
+      // console.log(req.body)
       create.save();
+
+      const notificationId =
+      await notificationController.createNotification({
+        user: req.body.client,
+        type: 0,
+        message: 'Tu pago se efectuó',
+      });
+
+      io.to(getSocketId_connected(req.body.client)).emit("addNotification", {
+        _id: notificationId,
+        message: 'Tu pago se efectuó',
+      });
+
+      const notificationSellerId =
+      await notificationController.createNotification({
+        user: req.body.seller,
+        type: 0,
+        message: 'Contrataron tu servicio',
+      });
+
+      io.to(getSocketId_connected(req.body.seller)).emit("addNotification", {
+        _id: notificationSellerId,
+        message: 'Contrataron tu servicio',
+      });
+
 
       res.status(201);
     } catch (error) {
