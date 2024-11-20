13d66d5206f00d95ee6a1d528531aafd3a2f5c68
Matias <103156161+assaffmatias@users.noreply.github.com>
2024-11-20 20:23:25 -0300

    response chat ia

--- controllers/oraculo.controller.js
+++ controllers/oraculo.controller.js
@@ -3,14 +3,13 @@ const { OpenAIClient } = require("../config");
 module.exports = {
   daily: async (req, res, next) => {
     try {
-      const { lastMessages, cartas } = req.body; // cartas:["El Mago", "Loco"] 
+      const { lastMessages, cartas } = req.body; // cartas: ["El Mago", "Loco"] 
       const params = {
         messages: [
           {
             role: "system",
             content:
-              `Eres un asistente que solo responde preguntas relacionadas con el horóscopo. Si te hacen una pregunta no relacionada con el horóscopo, debes responder: 'Lo siento, solo puedo responder preguntas relacionadas con el horóscopo. Ademas, ten en cuenta en cada respuesta que generes que ya le han tirado las cartas al usuario y son las siguientes: ${cartas.join(', ')}, responde detalladamente que significa cada una.`
-              ,
+              `Eres un asistente que responde preguntas relacionadas con el horóscopo. Ya le han tirado las cartas al usuario y son las siguientes: ${cartas.join(', ')}. Responde detalladamente qué significa cada una de ellas.`,
           },
           // {
           //   role: "user",
@@ -19,16 +18,21 @@ module.exports = {
         ],
         model: "gpt-3.5-turbo",
       };
+
+      // Si hay mensajes previos, añádelos a los parámetros
       if (Array.isArray(lastMessages) && lastMessages.length > 0) {
         params.messages = [...params.messages, ...lastMessages];
       }
+
       let response;
       try {
+        // Solicitar una respuesta del modelo de OpenAI
         response = await OpenAIClient.chat.completions.create(params);
       } catch (error) {
         response = null;
       }
 
+      // Devolver la respuesta obtenida
       return res.send({
         msg: "OK",
         response:
