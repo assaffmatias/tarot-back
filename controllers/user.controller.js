9e9941ff6ba3c566e0c391797544c9678f083e3a
Nicolas Buszczak <nico_947quil@hotmail.com>
2024-11-24 21:56:13 -0300

    User controller to get active chats and coins

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-const { User } = require("../models");
+const { User, Transaction } = require("../models");
 
 module.exports = {
   update: async (req, res, next) => {
@@ -12,6 +12,44 @@ module.exports = {
       return res.send({ msg: "Usuario actualizado", user });
     } catch (error) {
       next(error);
+    }
+  },
+  getUserData: async (req, res, next) => {
+    try {
+      const user = await User.findById(req.params.id)
+        .select(["-password", "-google", "-state"])
+        .lean()
+        .exec();
+        
+        if (!user) {
+          return res.status(404).send({ msg: "Usuario no encontrado" });
+        }
+        
+        const transactions = await Transaction.find({ client: user._id })
+        .populate({path:'payment', select: ['status', 'hiredUntil']})
+        .select([
+          "-client",
+          "-messages",
+          "-service",
+          "-price",
+          "-updatedAt",
+          "-createdAt"
+        ]) // Populate reader info (assuming there's a reader field)
+        .lean()
+        .exec();
+
+        const currentdate = new Date();
+        console.log(currentdate);
+        const filteredTransactions = transactions.filter(transaction =>
+          transaction.payment?.status === 'payed' && transaction?.payment.hiredUntil >= currentdate
+      );
+        console.log('filteredTransactions found:', filteredTransactions);
+        
+        
+        user.transactions = filteredTransactions;
+      return res.status(200).send({ user });
+    } catch (error) {
+      console.error(error);
     }
   },
 };
