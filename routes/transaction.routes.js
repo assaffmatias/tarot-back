fb9530d4064f65a2163c8157f91e662aa2d4a9fa
Nicolas Buszczak <nico_947quil@hotmail.com>
2024-11-24 22:01:10 -0300

    Added check for hired minutes

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -13,6 +13,7 @@ router.post(
     validateJWT,
     check("amount", "Debes enviar un valor").isString().notEmpty(),
     check("currency", "Debe enviarse un valor valido").isIn(["USD"]).notEmpty(),
+    check("hiredMinutes", "Debe enviarse un valor").isNumeric().isIn([1,60]).notEmpty()
   ],
   controller.newTransaction
 );
