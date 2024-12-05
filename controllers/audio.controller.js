34f98890ede831830a33cf8d4857b104c4d34790
Nicolas Buszczak <nico_947quil@hotmail.com>
2024-12-04 22:37:24 -0300

    basic audio controller

---
+++ controllers/audio.controller.js
@@ -0,0 +1,16 @@
+const bucket = require('../controllers/bucket.controller');
+
+module.exports = {
+    storeAudio: async (req, res) => {
+        //parse audio
+        try{
+            console.log(req.files)
+            const result = await bucket.putObject(req.files.audio.data);
+            res.status(200).json({ result });
+        }catch(error){
+            console.error(error);
+            return res.status(500).json({ error: error.message });
+        }
+    
+    }
+}
